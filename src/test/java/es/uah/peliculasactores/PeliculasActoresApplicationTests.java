7162f6fc5a45e68fec945935b4a2c4dde0e1edfb
Joel-Gomez-12 <hernandezjoel773@gmail.com>
2025-05-09 13:03:04 +0200

    Integración Sonar

--- src/test/java/es/uah/peliculasactores/PeliculasActoresApplicationTests.java
+++ src/test/java/es/uah/peliculasactores/PeliculasActoresApplicationTests.java
@@ -3,9 +3,14 @@ package es.uah.peliculasactores;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
 @ActiveProfiles("test")
+@TestPropertySource(properties = {
+        "AZURE_BLOB_CONNECTION_STRING=dummy-value",
+        "AZURE_BLOB_CONTAINER_NAME=dummy-container"
+})
 class PeliculasActoresApplicationTests {
 
     @Test
